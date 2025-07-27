d227ae36b9462fede1c0528ded9c577dec766829
강종근 <80893900+gangjonggeun@users.noreply.github.com>
2025-07-28 00:53:37 +0900

    align

--- lib/main_login_test.dart
+++ lib/main_login_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/theme/app_theme.dart';
-import 'features/login/presentation/screens/login_screen.dart';
+import 'features/auth/presentation/screens/login_screen.dart';
 
 void main() {
   runApp(
